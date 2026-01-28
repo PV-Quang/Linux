e0712c675e4ac2503da7c27bbb999374ab769432
PV-Quang <pvquang3054@gmail.com>
2026-01-28 14:28:36 +0700

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,134 +1,143 @@
-      #!/usr/bin/env bash
-      set -euo pipefail
+#!/usr/bin/env bash
+set -euo pipefail
 
-      exec > >(tee -a /var/log/k8s.log) 2>&1
+exec > >(tee -a /var/log/k8s.log) 2>&1
 
-      echo "[INFO] Start at $(date)"
+echo "[INFO] Start at $(date)"
 
-      rm -rf /etc/netplan/*
+rm -rf /etc/netplan/*
 
-      cat > /etc/netplan/k8s.yaml <<'EOF'
-      network:
-        version: 2
-        ethernets:
-          ens192:
-            dhcp4: false
-            dhcp6: false
-            addresses:
-              - 40.0.0.14/24
-            routes:
-              - to: default
-                via: 40.0.0.1
-            nameservers:
-              addresses:
-                - 40.0.0.250
-              search:
-                - pvq.lab
-      EOF
+cat > /etc/netplan/k8s.yaml <<'EOF'
+network:
+  version: 2
+  ethernets:
+    ens192:
+      dhcp4: false
+      dhcp6: false
+      addresses:
+        - 40.0.0.14/24
+      routes:
+        - to: default
+          via: 40.0.0.1
+      nameservers:
+        addresses:
+          - 40.0.0.250
+        search:
+          - pvq.lab
+EOF
 
-      netplan apply
+netplan apply
 
-      systemctl enable --now systemd-resolved.service
-      systemctl restart systemd-resolved.service
-      ln -sf /var/run/systemd/resolve/resolv.conf /etc/resolv.conf
+systemctl enable --now systemd-resolved.service
+systemctl restart systemd-resolved.service
+ln -sf /var/run/systemd/resolve/resolv.conf /etc/resolv.conf
 
-      hostnamectl set-hostname master-04.pvq.lab
-      timedatectl set-timezone Asia/Ho_Chi_Minh
+hostnamectl set-hostname master-04.pvq.lab
+timedatectl set-timezone Asia/Ho_Chi_Minh
 
-      echo "[INFO] Waiting for network..."
-      for i in $(seq 1 30); do
-        if ping -c1 -W1 8.8.8.8 >/dev/null 2>&1; then
-          echo "[INFO] Network OK"
-          break
-        fi
-        sleep 2
-      done
+echo "[INFO] Waiting for network..."
+for i in $(seq 1 30); do
+  if ping -c1 -W1 8.8.8.8 >/dev/null 2>&1; then
+    echo "[INFO] Network OK"
+    break
+  fi
+  sleep 2
+done
 
-      apt-get update -y
-      apt-get upgrade -y
-      apt-get install -y chrony
-      systemctl enable --now chrony
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y chrony
+systemctl enable --now chrony
 
-      cat > /etc/chrony/chrony.conf <<'EOF'
-      confdir /etc/chrony/conf.d
-      server 0.asia.pool.ntp.org
-      sourcedir /run/chrony-dhcp
-      sourcedir /etc/chrony/sources.d
-      keyfile /etc/chrony/chrony.keys
-      driftfile /var/lib/chrony/chrony.drift
-      ntsdumpdir /var/lib/chrony
-      logdir /var/log/chrony
-      maxupdateskew 100.0
-      rtcsync
-      makestep 1 3
-      leapsectz right/UTC
-      EOF
+cat > /etc/chrony/chrony.conf <<'EOF'
+confdir /etc/chrony/conf.d
+server 0.asia.pool.ntp.org
+sourcedir /run/chrony-dhcp
+sourcedir /etc/chrony/sources.d
+keyfile /etc/chrony/chrony.keys
+driftfile /var/lib/chrony/chrony.drift
+ntsdumpdir /var/lib/chrony
+logdir /var/log/chrony
+maxupdateskew 100.0
+rtcsync
+makestep 1 3
+leapsectz right/UTC
+EOF
 
-      systemctl restart chrony
-      systemctl enable chrony
-      sed -i "2i 127.0.1.1 $HOSTNAME" /etc/hosts
-      sed -i '3i\\' /etc/hosts
+systemctl restart chrony
+systemctl enable chrony
+sed -i "2i 127.0.1.1 $HOSTNAME" /etc/hosts
+sed -i '3i\\' /etc/hosts
 
-      swapoff -a
-      sed -i '/ swap / s/^/#/' /etc/fstab
-      echo -e "overlay\nbr_netfilter" | tee /etc/modules-load.d/containerd.conf
-      modprobe overlay
-      modprobe br_netfilter
-      cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
-      net.bridge.bridge-nf-call-iptables = 1
-      net.ipv4.ip_forward = 1
-      net.bridge.bridge-nf-call-ip6tables = 1
-      EOF
-      sysctl --system
+swapoff -a
+sed -i '/ swap / s/^/#/' /etc/fstab
+echo -e "overlay\nbr_netfilter" | tee /etc/modules-load.d/containerd.conf
+modprobe overlay
+modprobe br_netfilter
+cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sysctl --system
 
-      wget https://github.com/containerd/containerd/releases/download/v2.1.4/containerd-2.1.4-linux-amd64.tar.gz -P /tmp/
-      tar -C /usr/local -xzf /tmp/containerd-2.1.4-linux-amd64.tar.gz
-      wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service -O /etc/systemd/system/containerd.service
-      systemctl daemon-reload
-      systemctl enable --now containerd
-      wget https://github.com/opencontainers/runc/releases/download/v1.3.2/runc.amd64 -O /tmp/runc
-      install -m 755 /tmp/runc /usr/local/sbin/runc
-      wget https://github.com/containernetworking/plugins/releases/download/v1.8.0/cni-plugins-linux-amd64-v1.8.0.tgz -P /tmp/
-      mkdir -p /opt/cni/bin
-      tar -C /opt/cni/bin -xzf /tmp/cni-plugins-linux-amd64-v1.8.0.tgz
-      mkdir -p /etc/containerd
-      containerd config default | tee /etc/containerd/config.toml
-      sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
-      systemctl restart containerd
-      apt install -y apt-transport-https ca-certificates curl gpg
-      curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-      echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
-      apt update
-      apt install -y kubelet kubeadm kubectl
-      apt-mark hold kubelet kubeadm kubectl
-      
-
-      kubeadm init --pod-network-cidr 192.168.0.0/16 --service-cidr 10.96.0.0/12 --control-plane-endpoint master-04.pvq.lab --apiserver-cert-extra-sans 40.0.0.14 --apiserver-cert-extra-sans master-04 --apiserver-cert-extra-sans 61.14.236.249
-
-      sleep 10
-      mkdir -p /root/.kube
-      cp /etc/kubernetes/admin.conf /root/.kube/config
-      chown root:root /root/.kube/config
-
-      export KUBECONFIG=/etc/kubernetes/admin.conf
-      echo "[INFO] Waiting for Kubernetes API to be ready..."
-      for i in {1..60}; do
-        if kubectl get --raw=/readyz >/dev/null 2>&1; then
-          echo "[INFO] Kubernetes API is ready"
-          break
-        fi
-        echo "[INFO] API not ready yet ($i/60), retrying..."
-        sleep 5
-      done
-
-      echo "[INFO] Applying Calico CNI..."
-
-      kubectl apply --validate=false -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.0/manifests/tigera-operator.yaml
-
-      sleep 15
-
-      kubectl apply --validate=false -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.0/manifests/custom-resources.yaml
+wget https://github.com/containerd/containerd/releases/download/v2.1.4/containerd-2.1.4-linux-amd64.tar.gz -P /tmp/
+tar -C /usr/local -xzf /tmp/containerd-2.1.4-linux-amd64.tar.gz
+wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service -O /etc/systemd/system/containerd.service
+systemctl daemon-reload
+systemctl enable --now containerd
+wget https://github.com/opencontainers/runc/releases/download/v1.3.2/runc.amd64 -O /tmp/runc
+install -m 755 /tmp/runc /usr/local/sbin/runc
+wget https://github.com/containernetworking/plugins/releases/download/v1.8.0/cni-plugins-linux-amd64-v1.8.0.tgz -P /tmp/
+mkdir -p /opt/cni/bin
+tar -C /opt/cni/bin -xzf /tmp/cni-plugins-linux-amd64-v1.8.0.tgz
+mkdir -p /etc/containerd
+containerd config default | tee /etc/containerd/config.toml
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+systemctl restart containerd
+apt install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+apt update
+apt install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
 
 
-      sleep 10
-      echo "[INFO] Done at $(date)"
+kubeadm init --pod-network-cidr 192.168.0.0/16 --service-cidr 10.96.0.0/12 --control-plane-endpoint master-04.pvq.lab --apiserver-cert-extra-sans 40.0.0.14 --apiserver-cert-extra-sans master-04 --apiserver-cert-extra-sans 61.14.236.249
+
+sleep 10
+mkdir -p /root/.kube
+cp /etc/kubernetes/admin.conf /root/.kube/config
+chown root:root /root/.kube/config
+
+export KUBECONFIG=/etc/kubernetes/admin.conf
+echo "[INFO] Waiting for Kubernetes API to be ready..."
+for i in {1..60}; do
+  if kubectl get --raw=/readyz >/dev/null 2>&1; then
+    echo "[INFO] Kubernetes API is ready"
+    break
+  fi
+  echo "[INFO] API not ready yet ($i/60), retrying..."
+  sleep 5
+done
+
+echo "[INFO] Applying Calico CNI..."
+
+kubectl apply --validate=false -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.0/manifests/tigera-operator.yaml
+
+sleep 15
+
+kubectl apply --validate=false -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.0/manifests/custom-resources.yaml
+
+
+sleep 10
+echo "[INFO] Done at $(date)"
+
+runcmd:
+  - [ bash, -lc, "/usr/local/sbin/k8s.sh" ]
+
+power_state:
+  mode: reboot
+  message: "Reboot after cloud-init provisioning"
+  timeout: 60
+  condition: true
